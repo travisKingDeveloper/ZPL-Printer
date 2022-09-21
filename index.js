f95f11e8c4952208712bfcc4d49567a9dd39290c
Travis King <travis.king.developer@gmail.com>
2022-09-21 16:54:43 -0400

    push

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res) => {
+  console.log("req.headers", req.headers);
+  console.log("req.body", req.body);
+  console.log("req.url", req.url);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("running");
+});
