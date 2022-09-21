e46dcf7e97cccc581a2cdd4e1e0dea9a7a785d33
Travis King <travis.king.developer@gmail.com>
2022-09-21 17:01:21 -0400

    Stuff

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use((req, res) => {
   console.log("req.headers", req.headers);
   console.log("req.body", req.body);
   console.log("req.url", req.url);
+
+  res.json({ hello: "world" });
 });
 
 const port = process.env.PORT || 3000;
