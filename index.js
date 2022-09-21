15bfb9b729b31027398bdd11e18c338d22f1d015
Travis King <travis.king.developer@gmail.com>
2022-09-21 17:06:36 -0400

    Adding a get call and a standard bit

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log("req.headers", req.headers);
   console.log("req.body", req.body);
   console.log("req.url", req.url);
 
+  next();
+});
+
+app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
 
